d6d5ef6ee5562a9676188fbeccf56414934f4e3d
EwelinaChm <chmielewska@gmail.com>
2022-03-06 23:23:10 +0000

    Add code to play the game (ask questions, check answers, give 2 attempts for right answer, keep scores) and add function to run it

--- run.py
+++ run.py
@@ -16,10 +16,33 @@ SHEET = GSPREAD_CLIENT.open('scores')
 
 def intro_message():
     print("Welcome to LOVE TRIVIA game! \nAre you ready to test your knowledge?")
-    print("There are a total of 20 questions, you can skip a question anytime by typing 'skip', you have 3 attempts to give a correct answer")
+    print("There are a total of 20 questions, you can skip a question anytime by typing 'skip', you have 2 attempts to give a correct answer")
     input("Press any key to start the game")
     return True 
 
 
+def check_answer(question, answer, attempts, score):
+    if quiz[question]['answer'].lower() == ans.lower():
+        print(f"Correct Answer! \nYour score is {score + 1}!")
+        return True
+    else:
+        print(f"Wrong Answer :( \nYou have {attempts - 1} left! \nTry again...")
+        return False
+
+
 intro = intro_message()
-print(intro)
+while True:
+    score = 0
+    for question in quiz:
+        attempts = 2
+        while attempts > 0:
+            print(quiz[question]['question'])
+            answer = input("Your answer: ")
+            if answer == "skip":
+                break
+            check = check_answer(question, answer, attempts, score)
+            if check:
+                score += 1
+                break
+            attempts -= 1
+    break
